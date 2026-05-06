ee6946ec7d7ab946e34572a00d34ab7254b670bc
katiay13 <myarkatia@gmail.com>
2026-05-06 02:51:12 -0700

    Finished Maven project set up

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import ui.LandingPage;
-
-import javax.swing.*;
-
-public class Main {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new LandingPage().setVisible(true));
-    }
-}
